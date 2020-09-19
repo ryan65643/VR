c72741ff293eda617fae0672085be5c9bd2cea2e
MorrisCom <57181186+MorrisCom@users.noreply.github.com>
2020-09-19 16:59:49 +0800

    123

---
+++ VR/Assets/Script/DooR.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DooR : MonoBehaviour
+{
+    [Header("旋轉角度"), Range(0, 180)]
+    public float Angle = 90;
+    [Header("旋轉速度"), Range(0, 100)]
+    public float Speed = 1.5F;
+
+    public void Open()
+    {
+
+    }
+}
